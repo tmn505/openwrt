2b28e19cb00ebf99e2c4a5025657cdd7622cc4a5
Tomasz Maciej Nowak <tomek_n@o2.pl>
2020-06-16 00:55:27 +0200

    ath79: add support for Dell SonicPoint ACe APL26-0AE
    Dell/SonicWall APL26-0AE (marketed as SonicPoint ACe) is a dual band
    wireless access point.
    
    Specification
    SoC: QualcommAtheros QCA9550
    RAM: 256 MB DDR2
    Flash: 32 MB SPI NOR
    WIFI: 2.4 GHz 3T3R integrated
          5 GHz 3T3R QCA9890 oversized Mini PCIe card
    Ethernet: 2x 10/100/1000 Mbps QCA8334
              port labeled lan1 is PoE capable (802.3at)
    USB: 1x 2.0
    LEDs: 3x GPIO controlled
    Buttons: 2x GPIO controlled
    Serial: RJ-45 port, SonicWall pinout
            baud: 115200, parity: none, flow control: none
    
    Before flashing, be sure to have a copy of factory firmware, in case You
    wish to revert to original firmware.
    All described procedures were done in following environment:
    ROM Version: SonicROM (U-Boot) 8.0.0.0-11o
    SafeMode Firmware Version: SonicOS 8.0.0.0-14o
    Firmware Version: SonicOS 9.0.1.0
    In case of other versions, following installation instructions might be
    ineffective.
    
    Installation
    1. Prepare TFTP server with OpenWrt sysupgrade image and rename that
       image to "sp_fw.bin".
    2. Connect to one of LAN ports.
    3. Connect to serial port.
    4. Hold the reset button (small through hole on side of the unit),
       power on the device and when prompted to stop autoboot, hit any key.
       The held button can now be released.
    5. Alter U-Boot environment with following commands:
        setenv bootcmd bootm 0x9F510000
        saveenv
    6. Adjust "ipaddr" and "serverip" addresses in U-Boot environment, then
       run following command:
        run lf
    7. After successful flashing, execute:
        boot
    8. The access point will boot to OpenWrt. Wait few minutes, until the
       green "lan2" LED will stop blinking, then it's ready for
       configuration.
    
    Known issues
    Tool, 2.4GHz, 5GHz and lan1 green LEDs do not work.
    
    Signed-off-by: Tomasz Maciej Nowak <tomek_n@o2.pl>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -579,6 +579,19 @@ define Device/compex_wpj563
 endef
 TARGET_DEVICES += compex_wpj563
 
+define Device/dell_apl26-0ae
+  SOC := qca9550
+  DEVICE_VENDOR := Dell
+  DEVICE_MODEL := SonicPoint
+  DEVICE_VARIANT := ACe (APL26-0AE)
+  DEVICE_ALT0_VENDOR := SonicWall
+  DEVICE_ALT0_MODEL := SonicPoint
+  DEVICE_ALT0_VARIANT := ACe (APL26-0AE)
+  DEVICE_PACKAGES := ath10k-firmware-qca988x-ct kmod-ath10k-ct kmod-usb2
+  IMAGE_SIZE := 20480k
+endef
+TARGET_DEVICES += dell_apl26-0ae
+
 define Device/devolo_dvl1200e
   SOC := qca9558
   DEVICE_VENDOR := devolo
