06dc3e9c1af1cb1980c2dcf851c6d1a77e0a2475
Koen Vandeputte <koen.vandeputte@citymesh.com>
2023-04-12 11:33:51 +0200

    tegra: t210: add vic firmware
    This adds the required blob for the Video Image Compositor
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@citymesh.com>

--- package/firmware/linux-firmware/nvidia.mk
+++ package/firmware/linux-firmware/nvidia.mk
@@ -7,3 +7,11 @@ define Package/t210-usb-firmware/install
 endef
 $(eval $(call BuildPackage,t210-usb-firmware))
 
+Package/t210-vic-firmware = $(call Package/firmware-default,nVidia T210 VIC firmware)
+define Package/t210-vic-firmware/install
+	$(INSTALL_DIR) $(1)/lib/firmware/nvidia/tegra210
+	$(INSTALL_DATA) \
+		$(PKG_BUILD_DIR)/nvidia/tegra210/vic04_ucode.bin \
+		$(1)/lib/firmware/nvidia/tegra210
+endef
+$(eval $(call BuildPackage,t210-vic-firmware))
