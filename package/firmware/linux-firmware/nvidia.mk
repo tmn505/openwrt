3c22fd81e45090780e6423b5c614909ee17f50d0
Koen Vandeputte <koen.vandeputte@citymesh.com>
2023-04-12 11:34:15 +0200

    firmware: add support for nvidia gm20b (WIP)
    This commit allows to install the required firmware files
    for the gm20b gpu.
    
    This gpu is based on Maxwell and is present on Jetson Nano (t210).
    
    Signed-off-by: Koen Vandeputte <koen.vandeputte@citymesh.com>

--- package/firmware/linux-firmware/nvidia.mk
+++ package/firmware/linux-firmware/nvidia.mk
@@ -15,3 +15,23 @@ define Package/t210-vic-firmware/install
 		$(1)/lib/firmware/nvidia/tegra210
 endef
 $(eval $(call BuildPackage,t210-vic-firmware))
+
+Package/gm20b-firmware = $(call Package/firmware-default,GM20B Video Driver firmware)
+define Package/gm20b-firmware/install
+	$(INSTALL_DIR) $(1)/lib/firmware/nvidia/gm20b/acr
+	$(INSTALL_DIR) $(1)/lib/firmware/nvidia/gm20b/gr
+	$(INSTALL_DIR) $(1)/lib/firmware/nvidia/gm20b/pmu
+	$(CP) \
+	$(PKG_BUILD_DIR)/nvidia/gm20b/acr/*.bin \
+		$(1)/lib/firmware/nvidia/gm20b/acr
+	$(CP) \
+		$(PKG_BUILD_DIR)/nvidia/gm20b/gr/*.bin \
+		$(1)/lib/firmware/nvidia/gm20b/gr
+	$(CP) \
+		$(PKG_BUILD_DIR)/nvidia/gm200/gr/sw_method_init.bin \
+		$(1)/lib/firmware/nvidia/gm20b/gr
+	$(CP) \
+		$(PKG_BUILD_DIR)/nvidia/gm20b/pmu/*.bin \
+		$(1)/lib/firmware/nvidia/gm20b/pmu
+endef
+$(eval $(call BuildPackage,gm20b-firmware))
