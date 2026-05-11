tmn505/openwrt

commit d6d90566d1cd1cd6b7971064750b7ed05b8b524b

ath79: engenius,eXXX: use nvmem
Userspace handling is deprecated.

Move calibration for pcie node out of dtsi. calibration size for ath10k
and ath9k differs.

Signed-off-by: Rosen Penev <rosenp@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/16231
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/ath79/dts/qca9557_engenius_esr1200.dts
@@ -73,12 +73,20 @@
 	status = "okay";
 };
 
-&wmac {
-	nvmem-cells = <&calibration_art_1000>;
-	nvmem-cell-names = "calibration";
+&nvmem {
+	calibration_art_5000: calibration@5000 {
+		reg = <0x5000 0x844>;
+	};
 };
 
-&ath10k_0 {
-	nvmem-cells = <&calibration_art_5000>;
-	nvmem-cell-names = "calibration";
+&wmac {
+	nvmem-cells = <&calibration_art_1000>, <&macaddr_uboot_eth 1>;
+	nvmem-cell-names = "calibration", "mac-address";
+};
+
+&wifi0 {
+	compatible = "qcom,ath10k";
+
+	nvmem-cells = <&calibration_art_5000>, <&macaddr_uboot_eth 0>;
+	nvmem-cell-names = "calibration", "mac-address";
 };

target/linux/ath79/dts/qca9558_engenius_epg5000.dts
@@ -73,12 +73,20 @@
 	status = "okay";
 };
 
-&wmac {
-	nvmem-cells = <&calibration_art_1000>;
-	nvmem-cell-names = "calibration";
+&nvmem {
+	calibration_art_5000: calibration@5000 {
+		reg = <0x5000 0x844>;
+	};
 };
 
-&ath10k_0 {
-	nvmem-cells = <&calibration_art_5000>;
-	nvmem-cell-names = "calibration";
+&wmac {
+	nvmem-cells = <&calibration_art_1000>, <&macaddr_uboot_eth 1>;
+	nvmem-cell-names = "calibration", "mac-address";
+};
+
+&wifi0 {
+	compatible = "qcom,ath10k";
+
+	nvmem-cells = <&calibration_art_5000>, <&macaddr_uboot_eth 0>;
+	nvmem-cell-names = "calibration", "mac-address";
 };

target/linux/ath79/dts/qca9558_engenius_esr1750.dts
@@ -73,12 +73,20 @@
 	status = "okay";
 };
 
-&wmac {
-	nvmem-cells = <&calibration_art_1000>;
-	nvmem-cell-names = "calibration";
+&nvmem {
+	calibration_art_5000: calibration@5000 {
+		reg = <0x5000 0x844>;
+	};
 };
 
-&ath10k_0 {
-	nvmem-cells = <&calibration_art_5000>;
-	nvmem-cell-names = "calibration";
+&wmac {
+	nvmem-cells = <&calibration_art_1000>, <&macaddr_uboot_eth 1>;
+	nvmem-cell-names = "calibration", "mac-address";
+};
+
+&wifi0 {
+	compatible = "qcom,ath10k";
+
+	nvmem-cells = <&calibration_art_5000>, <&macaddr_uboot_eth 0>;
+	nvmem-cell-names = "calibration", "mac-address";
 };

target/linux/ath79/dts/qca9558_engenius_esr900.dts
@@ -73,18 +73,20 @@
 	status = "okay";
 };
 
-&wmac {
-	nvmem-cells = <&calibration_art_1000>;
-	nvmem-cell-names = "calibration";
+&nvmem {
+	calibration_art_5000: calibration@5000 {
+		reg = <0x5000 0x440>;
+	};
 };
 
-&pcie0 {
-	status = "okay";
+&wmac {
+	nvmem-cells = <&calibration_art_1000>, <&macaddr_uboot_eth 0>;
+	nvmem-cell-names = "calibration", "mac-address";
+};
 
-	wifi@0,0 {
-		compatible = "pci168c,0033";
-		reg = <0x0000 0 0 0 0>;
-		nvmem-cells = <&calibration_art_5000>;
-		nvmem-cell-names = "calibration";
-	};
+&wifi0 {
+	compatible = "pci168c,0033";
+
+	nvmem-cells = <&calibration_art_5000>, <&macaddr_uboot_eth 1>;
+	nvmem-cell-names = "calibration", "mac-address";
 };

target/linux/ath79/dts/qca955x_senao_router-dual.dtsi
@@ -30,6 +30,9 @@
 
 	phy-handle = <&phy0>;
 	pll-data = <0xa6000000 0x00000101 0x00001616>;
+
+	nvmem-cells = <&macaddr_uboot_eth 0>;
+	nvmem-cell-names = "mac-address";
 };
 
 &mdio0 {
@@ -43,8 +46,7 @@
 &pcie0 {
 	status = "okay";
 
-	ath10k_0: wifi@0,0 {
-		compatible = "qcom,ath10k";
+	wifi0: wifi@0,0 {
 		reg = <0x0000 0 0 0 0>;
 	};
 };
@@ -72,6 +74,16 @@
 				label = "u-boot-env";
 				reg = <0x030000 0x010000>;
 				read-only;
+
+				nvmem-layout {
+					compatible = "u-boot,env";
+					#address-cells = <1>;
+					#size-cells = <1>;
+
+					macaddr_uboot_eth: ethaddr {
+						#nvmem-cell-cells = <1>;
+					};
+				};
 			};
 
 			partition@40000 {
@@ -103,17 +115,13 @@
 				reg = <0xff0000 0x010000>;
 				read-only;
 
-				nvmem-layout {
+				nvmem: nvmem-layout {
 					compatible = "fixed-layout";
 					#address-cells = <1>;
 					#size-cells = <1>;
 
 					calibration_art_1000: calibration@1000 {
 						reg = <0x1000 0x440>;
-					};
-
-					calibration_art_5000: calibration@5000 {
-						reg = <0x5000 0x844>;
 					};
 				};
 			};

target/linux/ath79/generic/base-files/etc/board.d/02_network
@@ -737,7 +737,6 @@ ath79_setup_macs()
 	engenius,esr1200|\
 	engenius,esr1750|\
 	engenius,esr900)
-		lan_mac=$(mtd_get_mac_ascii u-boot-env ethaddr)
 		wan_mac=$(mtd_get_mac_ascii u-boot-env wanaddr)
 		;;
 	engenius,ews511ap)

target/linux/ath79/generic/base-files/etc/hotplug.d/ieee80211/10_fix_wifi_mac
@@ -52,12 +52,6 @@ case "$board" in
 		[ "$PHYNBR" -eq 0 ] && \
 			mtd_get_mac_ascii u-boot-env athaddr > /sys${DEVPATH}/macaddress
 		;;
-	engenius,epg5000|\
-	engenius,esr1200|\
-	engenius,esr1750|\
-	engenius,esr900)
-		macaddr_add "$(mtd_get_mac_ascii u-boot-env ethaddr)" "$PHYNBR" > /sys${DEVPATH}/macaddress
-		;;
 	engenius,ews511ap)
 		[ "$PHYNBR" -eq 0 ] && \
 		macaddr_add $(cat /sys/class/net/eth0/address) 1 > /sys${DEVPATH}/macaddress

target/linux/ath79/generic/base-files/lib/preinit/10_fix_eth_mac.sh
@@ -12,12 +12,6 @@ preinit_set_mac_address() {
 		ip link set dev eth0 address $(mtd_get_mac_ascii bdcfg "lanmac")
 		ip link set dev eth1 address $(mtd_get_mac_ascii bdcfg "wanmac")
 		;;
-	engenius,epg5000|\
-	engenius,esr1200|\
-	engenius,esr1750|\
-	engenius,esr900)
-		ip link set dev eth0 address $(mtd_get_mac_ascii u-boot-env ethaddr)
-		;;
 	siemens,ws-ap3610)
 		ip link set dev eth0 address $(mtd_get_mac_ascii cfg1 ethaddr)
 		;;

commit 9ff6bbfc982f61e50208a3c9b15d8e82f92fc416

ath79: engenius,eXXX: fix MAC assignment
Partially revert d6d90566d1cd. Unfortunately the ethaddr value in U-Boot
environment is enclosed in double quotes which makes it longer than
ETH_ALEN, thus nvmem returns EINVAL. Switch back to handling the MAC
addresses in user space.
While at it, specify proper environment size.

Fixes: d6d90566d1cd ("ath79: engenius,eXXX: use nvmem")
Fixes: https://github.com/openwrt/openwrt/issues/22963
Signed-off-by: Tomasz Maciej Nowak <tmn505@gmail.com>

target/linux/ath79/dts/qca9557_engenius_esr1200.dts
@@ -74,8 +74,8 @@
 };
 
 &wmac {
-	nvmem-cells = <&calibration_art_1000>, <&macaddr_uboot_eth 1>;
-	nvmem-cell-names = "calibration", "mac-address";
+	nvmem-cells = <&calibration_art_1000>;
+	nvmem-cell-names = "calibration";
 
 	led {
 		led-sources = <13>;
@@ -86,6 +86,6 @@
 &wifi0 {
 	compatible = "qcom,ath10k";
 
-	nvmem-cells = <&calibration_art_5000>, <&macaddr_uboot_eth 0>;
-	nvmem-cell-names = "calibration", "mac-address";
+	nvmem-cells = <&calibration_art_5000>;
+	nvmem-cell-names = "calibration";
 };

target/linux/ath79/dts/qca9558_engenius_epg5000.dts
@@ -74,8 +74,8 @@
 };
 
 &wmac {
-	nvmem-cells = <&calibration_art_1000>, <&macaddr_uboot_eth 1>;
-	nvmem-cell-names = "calibration", "mac-address";
+	nvmem-cells = <&calibration_art_1000>;
+	nvmem-cell-names = "calibration";
 
 	led {
 		led-sources = <13>;
@@ -86,6 +86,6 @@
 &wifi0 {
 	compatible = "qcom,ath10k";
 
-	nvmem-cells = <&calibration_art_5000>, <&macaddr_uboot_eth 0>;
-	nvmem-cell-names = "calibration", "mac-address";
+	nvmem-cells = <&calibration_art_5000>;
+	nvmem-cell-names = "calibration";
 };

target/linux/ath79/dts/qca9558_engenius_esr1750.dts
@@ -74,8 +74,8 @@
 };
 
 &wmac {
-	nvmem-cells = <&calibration_art_1000>, <&macaddr_uboot_eth 1>;
-	nvmem-cell-names = "calibration", "mac-address";
+	nvmem-cells = <&calibration_art_1000>;
+	nvmem-cell-names = "calibration";
 
 	led {
 		led-sources = <13>;
@@ -86,6 +86,6 @@
 &wifi0 {
 	compatible = "qcom,ath10k";
 
-	nvmem-cells = <&calibration_art_5000>, <&macaddr_uboot_eth 0>;
-	nvmem-cell-names = "calibration", "mac-address";
+	nvmem-cells = <&calibration_art_5000>;
+	nvmem-cell-names = "calibration";
 };

target/linux/ath79/dts/qca9558_engenius_esr900.dts
@@ -74,8 +74,8 @@
 };
 
 &wmac {
-	nvmem-cells = <&calibration_art_1000>, <&macaddr_uboot_eth 0>;
-	nvmem-cell-names = "calibration", "mac-address";
+	nvmem-cells = <&calibration_art_1000>;
+	nvmem-cell-names = "calibration";
 
 	led {
 		led-sources = <13>;
@@ -86,6 +86,6 @@
 &wifi0 {
 	compatible = "pci168c,0033";
 
-	nvmem-cells = <&calibration_art_5000>, <&macaddr_uboot_eth 1>;
-	nvmem-cell-names = "calibration", "mac-address";
+	nvmem-cells = <&calibration_art_5000>;
+	nvmem-cell-names = "calibration";
 };

target/linux/ath79/dts/qca955x_senao_router-dual.dtsi
@@ -30,9 +30,6 @@
 
 	phy-handle = <&phy0>;
 	pll-data = <0xa6000000 0x00000101 0x00001616>;
-
-	nvmem-cells = <&macaddr_uboot_eth 0>;
-	nvmem-cell-names = "mac-address";
 };
 
 &mdio0 {
@@ -77,12 +74,7 @@
 
 				nvmem-layout {
 					compatible = "u-boot,env";
-					#address-cells = <1>;
-					#size-cells = <1>;
-
-					macaddr_uboot_eth: ethaddr {
-						#nvmem-cell-cells = <1>;
-					};
+					env-size = <0x1000>;
 				};
 			};
 

target/linux/ath79/generic/base-files/etc/board.d/02_network
@@ -722,6 +722,7 @@ ath79_setup_macs()
 	engenius,esr1200|\
 	engenius,esr1750|\
 	engenius,esr900)
+		lan_mac=$(mtd_get_mac_ascii u-boot-env ethaddr)
 		wan_mac=$(mtd_get_mac_ascii u-boot-env wanaddr)
 		;;
 	hak5,lan-turtle|\

target/linux/ath79/generic/base-files/etc/hotplug.d/ieee80211/10_fix_wifi_mac
@@ -93,6 +93,15 @@ case "$board" in
 		[ "$PHYNBR" -eq 1 ] && \
 			mtd_get_mac_ascii devdata wlan24mac > /sys${DEVPATH}/macaddress
 		;;
+	engenius,epg5000|\
+	engenius,esr1200|\
+	engenius,esr1750|\
+	engenius,esr900)
+		[ "$PHYNBR" -eq 0 ] && \
+			macaddr_add "$(mtd_get_mac_ascii u-boot-env ethaddr)" 1 > /sys${DEVPATH}/macaddress
+		[ "$PHYNBR" -eq 1 ] && \
+			macaddr_add "$(mtd_get_mac_ascii u-boot-env ethaddr)" 0 > /sys${DEVPATH}/macaddress
+		;;
 	phicomm,k2t)
 		[ "$PHYNBR" -eq 0 ] && \
 			k2t_get_mac "5g_mac" > /sys${DEVPATH}/macaddress

target/linux/ath79/generic/base-files/lib/preinit/10_fix_eth_mac.sh
@@ -12,6 +12,12 @@ preinit_set_mac_address() {
 		ip link set dev eth0 address $(mtd_get_mac_ascii bdcfg "lanmac")
 		ip link set dev eth1 address $(mtd_get_mac_ascii bdcfg "wanmac")
 		;;
+	engenius,epg5000|\
+	engenius,esr1200|\
+	engenius,esr1750|\
+	engenius,esr900)
+		ip link set dev eth0 address $(mtd_get_mac_ascii u-boot-env ethaddr)
+		;;
 	siemens,ws-ap3610)
 		ip link set dev eth0 address $(mtd_get_mac_ascii cfg1 ethaddr)
 		;;