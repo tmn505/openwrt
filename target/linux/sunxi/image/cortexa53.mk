cf63d6623ce86e03e6d9b966c865774d610cf70c
Tomasz Maciej Nowak <tmn505@gmail.com>
2026-05-26 17:24:39 +0200

    sunxi: image: use global DTS variables
    No reason to keep SUNXI prefixed variables, since there are global
    equivalents.
    
    Signed-off-by: Tomasz Maciej Nowak <tmn505@gmail.com>

--- target/linux/sunxi/image/cortexa53.mk
+++ target/linux/sunxi/image/cortexa53.mk
@@ -7,7 +7,6 @@ KERNEL_LOADADDR:=0x40080000
 
 define Device/sun50i
   $(call Device/FitImageLzma)
-  SUNXI_DTS_DIR := allwinner/
   KERNEL_NAME := Image
 endef
 
@@ -66,7 +65,7 @@ define Device/libretech_all-h3-cc-h5
   DEVICE_MODEL := ALL-H3-CC
   DEVICE_VARIANT := H5
   $(Device/sun50i-h5)
-  SUNXI_DTS := $$(SUNXI_DTS_DIR)$$(SOC)-libretech-all-h3-cc
+  DEVICE_DTS := $$(SOC)-libretech-all-h3-cc
 endef
 TARGET_DEVICES += libretech_all-h3-cc-h5
 
@@ -75,7 +74,7 @@ define Device/olimex_a64-olinuxino
   DEVICE_MODEL := A64-Olinuxino
   DEVICE_PACKAGES := kmod-rtl8723bs rtl8723bu-firmware
   $(Device/sun50i-a64)
-  SUNXI_DTS := $$(SUNXI_DTS_DIR)$$(SOC)-olinuxino
+  DEVICE_DTS := $$(SOC)-olinuxino
 endef
 TARGET_DEVICES += olimex_a64-olinuxino
 
@@ -85,7 +84,7 @@ define Device/olimex_a64-olinuxino-emmc
   DEVICE_VARIANT := eMMC
   DEVICE_PACKAGES := kmod-rtl8723bs rtl8723bu-firmware
   $(Device/sun50i-a64)
-  SUNXI_DTS := $$(SUNXI_DTS_DIR)$$(SOC)-olinuxino-emmc
+  DEVICE_DTS := $$(SOC)-olinuxino-emmc
 endef
 TARGET_DEVICES += olimex_a64-olinuxino-emmc
 
